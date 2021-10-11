589777053272d1bd5c0696617783c299b7c36a5b
megha angadi <meghamangadi@gmail.com>
2021-10-11 19:59:31 +0530

    Added: Program to add contacts to the address book system

--- AddressBook/AddressBook.js
+++ AddressBook/AddressBook.js
@@ -1,7 +1,6 @@
 console.log("Welcome to the Address Book JavaScript Program");
 
-class Contact
-{
+class Contact {
     firstName;
     lastName;
     address;
@@ -11,8 +10,7 @@ class Contact
     phoneNo;
     email;
 
-    constructor(...params)
-    {
+    constructor(...params) {
         this.firstName = params[0];
         this.lastName = params[1];
         this.address = params[2];
@@ -23,72 +21,80 @@ class Contact
         this.email = params[7];
     }
 
-    get firstName()
-    { 
+    get firstName() {
         return this._firstName
     }
-    set firstName(firstName)
-    {
+    set firstName(firstName) {
         let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
-        if(nameRegex.test(firstName))
+        if (nameRegex.test(firstName))
             this._firstName = firstName
         else throw "Incorrect First Name"
     }
-    get lastName(){ return this._lastName}
-    set lastName(lastName){
+    get lastName() { return this._lastName }
+    set lastName(lastName) {
         let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
-        if(nameRegex.test(lastName))
+        if (nameRegex.test(lastName))
             this._lastName = lastName
         else throw "Incorrect Last Name"
     }
-    get address(){ return this._address}
-    set address(address){
+    get address() { return this._address }
+    set address(address) {
         let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
-        if(regex.test(address))
+        if (regex.test(address))
             this._address = address
         else throw "Incorrect Address"
     }
-    get city(){ return this._city}
-    set city(city){
+    get city() { return this._city }
+    set city(city) {
         let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
-        if(regex.test(city))
+        if (regex.test(city))
             this._city = city
         else throw "Incorrect City"
     }
-    get state(){ return this._state}
-    set state(state){
+    get state() { return this._state }
+    set state(state) {
         let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
-        if(regex.test(state))
+        if (regex.test(state))
             this._state = state
         else throw "Incorrect State"
     }
-    get zip(){ return this._zip}
-    set zip(zip){
+    get zip() { return this._zip }
+    set zip(zip) {
         let letterRegex = RegExp('^[^a-zA-z]+$')
         let splRegex = RegExp('^[!@#$^&%*()+=-[]\/{}|:<>?,.]+$')
         let pinRegex = RegExp('^[0-9]{3}[\\s]{0,1}[0-9]{3}$')
-        if(letterRegex.test(zip) && !splRegex.test(zip) && pinRegex.test(zip)){
-            zip = parseInt(zip.replace(" ",""))
+        if (letterRegex.test(zip) && !splRegex.test(zip) && pinRegex.test(zip)) {
+            zip = parseInt(zip.replace(" ", ""))
             this._zip = zip
         }
         else throw "Incorrect Zip"
     }
-    get phoneNo(){ return this._phoneNo}
-    set phoneNo(phoneNo){
+    get phoneNo() { return this._phoneNo }
+    set phoneNo(phoneNo) {
         let regex = RegExp('[0-9]{2}\\s[0-9]{10}')
-        if(regex.test(phoneNo))
+        if (regex.test(phoneNo))
             this._phoneNo = phoneNo
         else throw "Incorrect Phone Number"
     }
-    get email(){ return this._email}
-    set email(email){
+    get email() { return this._email }
+    set email(email) {
         let regex = RegExp('^[a-z]+([_+-.]?[a-z0-9]+)*[@][a-z0-9]+[.]([a-z]+){2,}([.]?[a-z]{2})?$')
-        if(regex.test(email))
+        if (regex.test(email))
             this._email = email
         else throw "Incorrect Email"
     }
 
 }
 
-let contact1 = new Contact("Megha", "Angadi", "Vidyagiri", "Bagalkot", "Karnataka",587102,9966337722,"megha@gmail.com");
-console.log(contact1)
+let contact1 = new Contact("Megha", "Angadi", "Vidyagiri", "Bagalkot", "Karnataka", "587102", "91 9278564345", "megha@gmail.com");
+let contact2 = new Contact("Gouri", "Math", "Navangar", "Gokak", "Goa", "549823", "91 9786347623", "gouri@gmail.com")
+let contact3 = new Contact("Sneha", "biradar", "MHNagar", "Dandeli", "Karnatka", "561067", "91 9687235598", "sneha@gmail.com")
+
+var addressBookArray = new Array()
+addressBookArray.push(contact1)
+addressBookArray.push(contact2)
+addressBookArray.push(contact3)
+
+for (let i = 0; i < addressBookArray.length; i++) {
+    console.log(addressBookArray[i].toString())
+}
